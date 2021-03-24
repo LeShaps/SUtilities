45b1351734eedd102aa82b8bfdb186fae00fa414
Paul Gaston <paul.gaston@sfr.fr>
2021-03-24 19:52:15 +0100

    [MOD/ADD] Add exception handling and unit tests for Query and Format Extensions

--- SUtilities/Extensions/QueryExtensions.cs
+++ SUtilities/Extensions/QueryExtensions.cs
@@ -5,21 +5,34 @@ namespace STools.Extensions
 {
     public static class QueryExtensions
     {
-        public static List<int> IndexesOf<T>(this List<T> List, T Comparison)
+        public static ICollection<int> IndexesOf<T>(this List<T> List, T Comparison)
             where T : class
         {
+            if (List is null) {
+                throw new ArgumentNullException(nameof(List));
+            } else if (Comparison is null) {
+                throw new ArgumentNullException(nameof(Comparison));
+            }
+
             List<int> Indexes = new();
 
             for (int i = 0; i < List.Count; i++) {
-                if (List[i] == Comparison)
+                if (List[i] == Comparison) {
                     Indexes.Add(i);
+                }
             }
 
             return Indexes;
         }
 
-        public static List<int> IndexesOf<T>(this List<T> List, Func<T, bool> Predicate)
+        public static ICollection<int> IndexesOf<T>(this List<T> List, Func<T, bool> Predicate)
         {
+            if (List is null) {
+                throw new ArgumentNullException(nameof(List));
+            } else if (Predicate is null) {
+                throw new ArgumentNullException(nameof(Predicate));
+            }
+
             List<int> Indexes = new();
 
             for (int i = 0; i < List.Count; i++) {
---
+++ SUtilitiesTests/Extensions/QueryExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using STools.Extensions;
+
+namespace SUtilitiesTests
+{
+    [TestClass]
+    public class QueryExtensionsTests
+    {
+        [TestMethod]
+        public void IndexesOfRawTest()
+        {
+            string NullString = null;
+
+            List<string> StringList = new()
+            {
+                "The",
+                "Quick",
+                "Fox",
+                "Is",
+                "Quick"
+            };
+
+            List<string> NullList = null;
+
+            List<int> StringIndexResult = StringList.IndexesOf("Quick").ToList();
+            Assert.AreEqual(2, StringIndexResult.Count);
+            Assert.IsTrue(StringIndexResult.Contains(1));
+            Assert.IsTrue(StringIndexResult.Contains(4));
+            Assert.ThrowsException<ArgumentNullException>(() => StringList.IndexesOf(NullString));
+            StringIndexResult = StringList.IndexesOf("dog").ToList();
+            Assert.AreEqual(0, StringIndexResult.Count);
+            Assert.ThrowsException<ArgumentNullException>(() => NullList.IndexesOf("Quick"));
+        }
+
+        [TestMethod]
+        public void IndexesOfPredicateTest()
+        {
+            List<string> StringList = new()
+            {
+                "The",
+                "Quick",
+                "Fox",
+                "Is",
+                "Quick",
+                "Or",
+                "Quicker"
+            };
+
+            List<string> NullList = null;
+            Func<string, bool> predicate = null;
+
+            List<int> StringIndexResult = StringList.IndexesOf(x => x.StartsWith('Q')).ToList();
+            Assert.AreEqual(3, StringIndexResult.Count);
+            StringIndexResult = StringList.IndexesOf(x => x.Length > 900).ToList();
+            Assert.AreEqual(0, StringIndexResult.Count);
+            Assert.ThrowsException<ArgumentNullException>(() => NullList.IndexesOf(x => x.StartsWith('Q')));
+            Assert.ThrowsException<ArgumentNullException>(() => StringList.IndexesOf(predicate));
+        }
+    }
+}
